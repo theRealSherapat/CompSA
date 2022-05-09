33c28bcae27a0b8d15a078196e1adea691c3a727
theRealSherapat <thorvaldsend@protonmail.com>
2022-05-10 00:28:28 +0200

    Cleaned up more in the Experiments and results chapter, as well as inserted the last Nymoen reproducing experiment. I might have to comment more on whether Nyomen's results were seen in my own, and comment more on average harmonic synchronization times than I currently do (analyze experiment results a bit more). Now, after you do the simpler Mirollo-Strogatz vs. Nymoen phase adjustment—and possibly write some analysis from the reMarkable after including the results in the thesis report—move onto the cooler experiments, but also remember you're getting quite bad with time to shine up your text and arrive at a first draft for others to read and give feedback on.

---
+++ Synchrony/run_onedirectional_vs_bidirectional_phase_synchronization_experiment.py
@@ -0,0 +1,30 @@
+from experiment_running_utils import *
+
+def main():
+    """ Sets up and executes plans for all the wanted experiment data samples we want to collect. """
+    
+    """ Experiment setup and execution plan (can contain several experiments and run sequentially, but then you have to split up the dataset manually after it): """
+    
+    collsizes = [6, 25, 50, 100]
+	
+    # FOR MIROLLO-STROGATZ's PHASE ADJ. METHOD:
+    for collsize_val in collsizes:
+        # Collecting data sample:
+        # Hyperparameter / covariate assigning (currently just for homogenous robots).
+        assignNonDefaultHyperparameters(collsize=collsize_val, alpha=FINNDENBESTEALPHAEN, beta=FINNDENBESTEBETAEN, nymoenrec=0, trefperc=FINNDENBESTETREFPERCEN, phaseadj=0, beta=0, freqadj=0)
+        # Datasample collecting / no_of_runs simulation runs being executed in parrallell.
+        runNumberOfSimulationRuns(sampSize)
+     
+     # FOR NYMOEN's PHASE ADJ. METHOD:
+     for collsize_val in collsizes:
+        # Collecting data sample:
+        # Hyperparameter / covariate assigning (currently just for homogenous robots).
+        assignNonDefaultHyperparameters(collsize=collsize_val, alpha=FINNDENBESTEALPHAEN, beta=FINNDENBESTEBETAEN, nymoenrec=0, trefperc=FINNDENBESTETREFPERCEN, phaseadj=1, beta=0, freqadj=0)
+        # Datasample collecting / no_of_runs simulation runs being executed in parrallell.
+        runNumberOfSimulationRuns(sampSize)
+
+
+if __name__ == "__main__":
+    """ Fully automatic experiments script (although homogenous and saving to one synchronyDataset.csv file). """
+    
+    main()
