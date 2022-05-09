f7306facd64a08cfa447906f74850da23dcb4035
theRealSherapat <thorvaldsend@protonmail.com>
2022-05-09 15:43:12 +0200

    Shone a bit more the Experiments and results chapter after last experiment inclusion, and found out I want to use alpha = 0.2 based on all other experiments for my next (potentially two next) experiments.

--- Experiments/Execute (although not here)/Specific/run_experiment_with_best_alpha_for_betas.py
+++ Experiments/Execute (although not here)/Specific/run_experiment_with_best_alpha_for_betas.py
@@ -7,9 +7,9 @@ def main():
     
     betas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
     for beta_val in betas:
-        # Collecting one data sample per Alpha:
         # Hyperparameter / covariate assigning (currently just for homogenous robots).
-        assignNonDefaultHyperparameters(beta=beta_val,  collsize=6, nymoenrec=1, alpha=FINN_DEN_BESTE_ALPHAEN_HITTIL_FRA_FORRIGE_EKSPERIMENT, phaseadj=1, freqadj=1)
+        assignNonDefaultHyperparameters(beta=beta_val,  collsize=6, nymoenrec=1, alpha=0.2, phaseadj=1, freqadj=1)
+        
         # Datasample collecting / no_of_runs simulation runs being executed in parrallell.
         runNumberOfSimulationRuns(sampSize)
 
